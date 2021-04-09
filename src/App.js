f4668606cb5bf296f47a0c5b3d190ec7c49cc674
Laurie24 <villeneuvelaurie@orange.fr>
2021-04-09 17:23:40 +0200

    :tada: project implementation with reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Header from "./components/header/Header";
+import MovieList from "./components/movie-list/MovieList";
+import MovieDetails from "./components/movie-details/MovieDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+        <div className="App">
+          <Header/>
+          <MovieList/>
+          <MovieDetails/>
+        </div>
+    );
+  }
 }
 
 export default App;
