13e299182feb2827e89681b2445b7e8b4295e865
Laurie24 <villeneuvelaurie@orange.fr>
2021-04-09 18:02:09 +0200

    :sparkles: create component detail and movie-list

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import MovieDetails from "./components/movie-details/MovieDetails";
 class App extends Component{
   render() {
     return (
-        <div className="App">
-          <Header/>
-          <div className='d-flex flex-row'>
-              <MovieList/>
-              <MovieDetails/>
-          </div>
+        <div className="App d-flex flex-column">
+            <Header />
+            <div className="d-flex flex-row flex-fill pt-4 p-2" >
+                <MovieList />
+                <MovieDetails />
+            </div>
         </div>
     );
   }
