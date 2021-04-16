fb621850145f500ee3cd797e31a2cca75371b65b
Laurie24 <villeneuvelaurie@orange.fr>
2021-04-16 16:02:42 +0200

    :sparkles: add selected-movie

--- src/App.js
+++ src/App.js
@@ -4,13 +4,48 @@ import MovieList from "./components/movie-list/MovieList";
 import MovieDetails from "./components/movie-details/MovieDetails";
 
 class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies : [{
+                title: 'Gladiator',
+                img: 'https://www.gstatic.com/tv/thumb/v22vodart/24674/p24674_v_v8_ag.jpg',
+                details: 'R | 155min | Action',
+                description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+                {
+                    title: 'La belle et la bête',
+                    img: 'https://www.gstatic.com/tv/thumb/v22vodart/24674/p24674_v_v8_ag.jpg',
+                    details: 'R | 155min | Dessin animé',
+                    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                },
+                {
+                    title: 'Le roi lion',
+                    img: 'https://www.gstatic.com/tv/thumb/v22vodart/24674/p24674_v_v8_ag.jpg',
+                    details: 'R | 155min | Dessin animé',
+                    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                }
+            ],
+            selectedMovie: 0
+        }
+    }
+
+    updateSelectedMovie = (title) => {
+        const index = this.state.movies.findIndex((m) => {
+            return title === m.title;
+        })
+        this.setState({
+            selectedMovie: index
+        })
+    }
+
   render() {
     return (
         <div className="App d-flex flex-column">
             <Header />
             <div className="d-flex flex-row flex-fill pt-4 p-2" >
-                <MovieList />
-                <MovieDetails />
+                <MovieList movies={this.state.movies} updateSelectedMovie={this.updateSelectedMovie} />
+                <MovieDetails movie={this.state.movies[this.state.selectedMovie]}/>
             </div>
         </div>
     );
--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -2,6 +2,17 @@ import React, {Component} from 'react'
 
 export default class MovieDetails extends Component{
     render() {
-        return <div className='w-25 p-4'>Movie détails</div>
+        return (
+            <div className='w-25 p-4 d-flex flex-column'>
+                <h5>{this.props.movie.title}</h5>
+                <hr className={'w-100'}/>
+                <div className={'d-flex justify-content-center'}>
+                    <img width={'150'} src={this.props.movie.img} alt={this.props.movie.title}/>
+                </div>
+                <hr className={'w-100'}/>
+                <p className={'text-secondary'}>{this.props.movie.details}</p>
+                <p>{this.props.movie.description}</p>
+            </div>
+        )
     }
 }
